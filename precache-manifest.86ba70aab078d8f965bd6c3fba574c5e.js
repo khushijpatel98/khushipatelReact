7517d912ffead6d610af1a81feda62fa29ce4450
Khushi Patel <khushijpatel98@gmail.com>
2020-06-17 11:12:45 +0530

    Updates

---
+++ precache-manifest.86ba70aab078d8f965bd6c3fba574c5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67ee50725b2ddcd5c1844b4e5869a2ce",
+    "url": "/khushipatelReact/index.html"
+  },
+  {
+    "revision": "6bc2bfbe8357619fa0f8",
+    "url": "/khushipatelReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36f037ca2a3c7fd8e117",
+    "url": "/khushipatelReact/static/js/2.b072d574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/khushipatelReact/static/js/2.b072d574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc2bfbe8357619fa0f8",
+    "url": "/khushipatelReact/static/js/main.ab97258e.chunk.js"
+  },
+  {
+    "revision": "cd2a931629f9e80b1e78",
+    "url": "/khushipatelReact/static/js/runtime-main.f0a4d48b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/khushipatelReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
